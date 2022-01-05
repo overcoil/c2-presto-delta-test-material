47c2819b813830ac187bac11548e002a472df512
George Chow <george@overcoil.com>
2022-01-05 10:42:11 -0800

    re-org of content

--- extract.sh
+++ extract.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sed s/ZZ/$1/g extract-param.awk > extract.awk
-awk -f extract.awk tpcdsq-raw/tpcds-ansi-all.sql > tpcdsq-raw/q$1.sql
-cat rtu/delta-cte.sql tpcdsq-raw/q$1.sql > rtu/q$1.sql
+sed s/ZZ/$1/g extract-param.awk > .extract.awk
+awk -f .extract.awk raw/tpcds-ansi-all.sql > raw/q$1.sql
+cat rtu/delta-cte.sql raw/q$1.sql > rtu/q$1.sql
 ls -l rtu/q$1.sql
 echo Review your query before use.
