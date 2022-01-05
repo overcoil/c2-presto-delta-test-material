acaac10c1e5dfb041bf88d081caed7e289169edd
George Chow <george@overcoil.com>
2022-01-05 10:47:47 -0800

    add notes for posterity

--- extract.sh
+++ extract.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+#
+# usage: extract.sh N
+#  N is the index (1-99) from the TPC-DS query set
+#
+# delta-cte.sql is populated fragment that points to your S3 bucket where the TPC-DS dataset resides
+# 
+# the output is saved at rtu/qN.sql
+# 
 sed s/ZZ/$1/g extract-param.awk > .extract.awk
 awk -f .extract.awk raw/tpcds-ansi-all.sql > raw/q$1.sql
 cat rtu/delta-cte.sql raw/q$1.sql > rtu/q$1.sql
